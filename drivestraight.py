8b8a7b58f5524ee2c919ec1511ad28c6eec9a272
Ann Song <gmsong1978@gmail.com>
2023-04-17 12:16:52 -0400

    Smoother without proportional control

--- drivestraight.py
+++ drivestraight.py
@@ -7,12 +7,10 @@ class DriveStraight:
         print(f"RIGHT: {self.drivetrain.encoder_right.getDistance()}, LEFT: {self.drivetrain.encoder_left.getDistance()}")
         difference = self.drivetrain.encoder_right.getDistance() - self.drivetrain.encoder_left.getDistance()
         if difference > 0.1:
-            print("TURNING RIGHT")
-            self.drivetrain.arcadeDrive(0.3, 0.5)
-        elif difference < -0.1:
             print("TURNING LEFT")
-            self.drivetrain.arcadeDrive(-0.4, 0.5)
+            self.drivetrain.arcadeDrive(0.3**2, 0.5)
+        elif difference < -0.1:
+            print("TURNING RIGHT")
+            self.drivetrain.arcadeDrive(-0.3**2, 0.5)
         else:
             self.drivetrain.arcadeDrive(0, 0.5)
-
-        # self.drivetrain.arcadeDrive(difference, 0.5)  # TODO: turn left if difference is positive, turn right if difference is negative
