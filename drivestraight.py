f09f4050cec6c7a395bf9e30bdb2c4706b8b1620
Ann Song <gmsong1978@gmail.com>
2023-04-17 12:10:01 -0400

    Working drive straight

--- drivestraight.py
+++ drivestraight.py
@@ -3,5 +3,16 @@ class DriveStraight:
         self.drivetrain = drivetrain
 
     def run(self):
-        difference = self.drivetrain.encoder_left.getDistance() - self.drivetrain.encoder_right.getDistance()
-        self.drivetrain.arcadeDrive(difference, 0.5)  # TODO: turn left if difference is positive, turn right if difference is negative
+        self.drivetrain.move_forward(0.5)
+        print(f"RIGHT: {self.drivetrain.encoder_right.getDistance()}, LEFT: {self.drivetrain.encoder_left.getDistance()}")
+        difference = self.drivetrain.encoder_right.getDistance() - self.drivetrain.encoder_left.getDistance()
+        if difference > 0.1:
+            print("TURNING RIGHT")
+            self.drivetrain.arcadeDrive(0.3, 0.5)
+        elif difference < -0.1:
+            print("TURNING LEFT")
+            self.drivetrain.arcadeDrive(-0.4, 0.5)
+        else:
+            self.drivetrain.arcadeDrive(0, 0.5)
+
+        # self.drivetrain.arcadeDrive(difference, 0.5)  # TODO: turn left if difference is positive, turn right if difference is negative
--- robot.py
+++ robot.py
@@ -1,15 +1,21 @@
 from wpilib import TimedRobot
 from drivetrain import Drivetrain
-from wpilib import Spark, Encoder
+from wpilib import Spark, Encoder, Joystick
+from drivestraight import DriveStraight
+import wpilib
+import os
 
 class MyRobot(TimedRobot):
     def robotInit(self):
+        self.controller = Joystick(0)
         self.drivetrain = Drivetrain(Spark(0), Spark(1), Encoder(4, 5), Encoder(6, 7))
         self.drivetrain.encoder_right.setDistancePerPulse(0.0183)
         self.drivetrain.encoder_left.setDistancePerPulse(0.0183)
 
         self.drivetrain.encoder_right.reset()
         self.drivetrain.encoder_left.reset()
+
+        self.drive_straight = DriveStraight(self.drivetrain)
 
     def robotPeriodic(self):
         pass
@@ -18,10 +24,18 @@ class MyRobot(TimedRobot):
         pass
 
     def autonomousPeriodic(self):
-        pass
+        self.drive_straight.run()
 
     def teleopInit(self):
         pass
 
     def teleopPeriodic(self):
-        pass
+        forward = self.controller.getRawAxis(0)
+        turn = self.controller.getRawAxis(1)
+        self.drivetrain.arcadeDrive(forward, turn)
+
+
+if __name__ == "__main__":
+    os.environ["HALSIMWS_HOST"] = "10.0.0.2"
+    os.environ["HALSIMWS_PORT"] = "3300"
+    wpilib.run(MyRobot)
