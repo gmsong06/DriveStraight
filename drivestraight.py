e9e3e2c82cee8918ffc7a341dae7ae686d9fe881
Ann Song <gmsong1978@gmail.com>
2023-04-17 12:25:21 -0400

    Stops at 2 meters

--- drivestraight.py
+++ drivestraight.py
@@ -3,14 +3,16 @@ class DriveStraight:
         self.drivetrain = drivetrain
 
     def run(self):
-        self.drivetrain.move_forward(0.5)
         print(f"RIGHT: {self.drivetrain.encoder_right.getDistance()}, LEFT: {self.drivetrain.encoder_left.getDistance()}")
         difference = self.drivetrain.encoder_right.getDistance() - self.drivetrain.encoder_left.getDistance()
-        if difference > 0.1:
-            print("TURNING LEFT")
-            self.drivetrain.arcadeDrive(0.3**2, 0.5)
-        elif difference < -0.1:
-            print("TURNING RIGHT")
-            self.drivetrain.arcadeDrive(-0.3**2, 0.5)
+        if (self.drivetrain.encoder_right.getDistance() + self.drivetrain.encoder_left.getDistance())/2 >= 200:
+            self.drivetrain.arcadeDrive(0, 0)
         else:
-            self.drivetrain.arcadeDrive(0, 0.5)
+            if difference > 0.1:
+                print("TURNING LEFT")
+                self.drivetrain.arcadeDrive(0.3**2, 0.5)
+            elif difference < -0.1:
+                print("TURNING RIGHT")
+                self.drivetrain.arcadeDrive(-0.3**2, 0.5)
+            else:
+                self.drivetrain.arcadeDrive(0, 0.5)
